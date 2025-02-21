e3e6999bafe9c8a3c22d7594701d3c2e7d35f637
Paulo Sandim <pauloedusandim@gmail.com>
2025-02-21 19:34:20 -0300

    encapsulamento com pom

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,54 +1,43 @@
 import { test, expect } from '@playwright/test';
 import { obterCodigo2FA } from '../support/db';
+import { LoginPage } from '../pages/LoginPage';
 
 test('Não deve logar quando o código de autenticação é inválido', async ({ page }) => {
+  const loginPage = new LoginPage(page)
+
   const usuario = {
     cpf: '00000014141',
     senha: '147258'
   }
   
-  await page.goto('http://paybank-mf-auth:3000/');
-
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  for (const digito of usuario.senha) {
-    await page.getByRole('button', { name: digito }).click();
-  }
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  await page.getByRole('textbox', { name: '000000' }).fill('123456');
-  await page.getByRole('button', { name: 'Verificar' }).click();
+  await loginPage.acessaPagina()
+  await loginPage.informaCPF(usuario.cpf)
+  await loginPage.informaSenha(usuario.senha)
+  await loginPage.informa2FA('123456')
 
   await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
 });
 
 test('Deve acessar a conta do usuário', async ({ page }) => {
+  const loginPage = new LoginPage(page)
+
   const usuario = {
     cpf: '00000014141',
     senha: '147258'
   }
   
-  await page.goto('http://paybank-mf-auth:3000/');
-
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  for (const digito of usuario.senha) {
-    await page.getByRole('button', { name: digito }).click();
-  }
-  await page.getByRole('button', { name: 'Continuar' }).click();
+  await loginPage.acessaPagina()
+  await loginPage.informaCPF(usuario.cpf)
+  await loginPage.informaSenha(usuario.senha)
 
   // temporario
   await page.waitForTimeout(3000)
+  const codigo = await obterCodigo2FA()
 
-  const code = await obterCodigo2FA()
-
-  await page.getByRole('textbox', { name: '000000' }).fill(code);
-  await page.getByRole('button', { name: 'Verificar' }).click();
+  await loginPage.informa2FA(codigo)
 
   // temporario
   await page.waitForTimeout(2000)
 
-  await expect(page.locator('#account-balance')).toContainText('R$ 5.000,00');
+  expect(await loginPage.obterSaldo()).toHaveText('R$ 5.000,00')
 });
